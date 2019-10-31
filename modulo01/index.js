65dc25473e5d215c23fb3cccf7af6f43d28ec509
cesar.lima <cesar.lima@etapa.com.br>
2019-10-31 09:10:52 -0300

    informações sobre Middleware

--- modulo01/index.js
+++ modulo01/index.js
@@ -6,25 +6,52 @@ server.use(express.json);
 
 const users = ['user1','user2','user3'];
 
+server.use((req, res, next) => {
+    console.time('Request');
+    console.log(`Método: ${req.method}; URL: ${req.url};`);
+
+    next();
+    console.timeEnd('Request');
+});
+
+function checkUsersExists(req, res, next){
+    if(!req.body.user){
+        return res.status(400).json({error: 'User name is required'});
+    }
+    return next();
+}
+
+function checkUserInArray(req, res, next){
+    const user = users[req.params.index];
+    if(!user){
+        return res.status(400).json({error: 'User does not exists'});
+    }
+    req.user = user;
+
+
+    return next();
+}
 server.get('/users', (req, res) =>{
    
     return res.json(users);
 });
 
-server.get('/users/:index', (req, res) =>{
-    const { index } = req.params;
-
-    return res.json(users[index]);
+server.get('/users/:index',checkUserInArray, (req, res) =>{
+    
+    /*const { index } = req.params;
+    return res.json(users[index]);*/
+    //por que ja tem a variavel no middleware pode acessar igual abaixo
+    return res.json(req.user);
 });
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUsersExists, (req, res) => {
     const { name } = req.body;
 
     users.push( name );
     return res.json( users );
 });
 
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUsersExists, checkUserInArray, (req, res) => {
     const { index } = req.params;
     const { name }  = req.body;
 
@@ -32,7 +59,7 @@ server.put('/users/:index', (req, res) => {
 
     return res.json( users );
 });
-server.delete('/users/:index', (req, res) =>{
+server.delete('/users/:index', checkUserInArray, (req, res) =>{
     const { index } = req.params;
     users.splice(index, 1);
 
