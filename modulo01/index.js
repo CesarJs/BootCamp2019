5f6f188aa55059800e4345550bad57dd1b9fe377
cesar.lima <cesar.lima@etapa.com.br>
2019-10-31 08:44:08 -0300

    Crud básico

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,12 +2,43 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json);
 
-server.get('/users/:id', (req, res) =>{
-    //const nome = req.query.nome;
-    //const { id } = req.params;
-    const { id }  = req.params;
-    return res.json({message: `Buscando usuário  ${id}`});
+const users = ['user1','user2','user3'];
+
+server.get('/users', (req, res) =>{
+   
+    return res.json(users);
 });
+
+server.get('/users/:index', (req, res) =>{
+    const { index } = req.params;
+
+    return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+
+    users.push( name );
+    return res.json( users );
+});
+
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params;
+    const { name }  = req.body;
+
+    users[index]    = name;
+
+    return res.json( users );
+});
+server.delete('/users/:index', (req, res) =>{
+    const { index } = req.params;
+    users.splice(index, 1);
+
+    return res.send();
+});
+
+
 
 server.listen(3000);
