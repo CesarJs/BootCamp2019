eb9d1d5ae7e36e3f5ac3f8139119b41184afd55e
CesarJs <csr2314@gmail.com>
2019-12-04 22:00:04 -0300

    mais um pouco

--- gobarber/src/app.js
+++ gobarber/src/app.js
@@ -1,20 +1,22 @@
-import express from 'express';
-import routes  from './routes';
+import express from "express";
+import routes from "./routes";
 
-class App{
-    constructor(){
-        this.server = express();
-        this.middlewares();
-        this.routes();
-    }
+import "./database";
 
-    middlewares(){
-        this.server.use(express.json());
-    }
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
 
-    routes(){
-        this.server.use(routes);
-    }
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,8 +1,14 @@
-import { Router } from 'express';
+import { Router } from "express";
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({message: 'Hello World'});
-})
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    name: "Cesar Augusto",
+    email: "csr2314@gmail.com",
+    password_hash: "CesarEsas"
+  });
+  return res.json(user);
+});
 export default routes;
